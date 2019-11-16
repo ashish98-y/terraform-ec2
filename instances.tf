489de609daadb35b2e074d900c6da77761bf0730
ashish <itt.ashish@gmail.com>
2019-11-16 12:10:00 +0530

    install ansible in workspace

--- instances.tf
+++ instances.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "dbserver" {
   #ami="${data.aws_ami.ubuntu.id}"
   ami           = "${lookup(var.amiId, var.AWS_REGION)}"
   instance_type = "t2.micro"
+  user_data="${file(user-data.sh)}"
   key_name="test"
 }
 
---
+++ user-data.sh
@@ -0,0 +1,7 @@
+apt install software-properties-common
+apt-add-repository ppa:ansible/ansible -y
+apt-get update
+apt-get upgrade -y
+apt install python
+apt install ansible
+#conf ssh connection
