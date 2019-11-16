f5d3951d961032fdab6ac04f26a19c61710c3fab
ashish <itt.ashish@gmail.com>
2019-11-16 14:16:52 +0530

    user data syntaxwq

--- instances.tf
+++ instances.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "dbserver" {
   ami           = "${lookup(var.amiId, var.AWS_REGION)}"
   instance_type = "t2.micro"
 vpc_security_group_ids="${aws_security_group.asg.id}"
-  user_data="${file("${path.cwd}/user-data.sh")}"
+  user_data="${file("${path.module}/user-data.sh")}"
   key_name="test"
 
 }
