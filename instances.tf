b03008719f05887cafc48c0e06131e5af1fcd225
ashish <itt.ashish@gmail.com>
2019-11-16 12:27:03 +0530

    security group

--- instances.tf
+++ instances.tf
@@ -2,8 +2,10 @@ resource "aws_instance" "dbserver" {
   #ami="${data.aws_ami.ubuntu.id}"
   ami           = "${lookup(var.amiId, var.AWS_REGION)}"
   instance_type = "t2.micro"
+vpc_security_group_ids="${aws_security_group.asg.id}"
   user_data="${file(user-data.sh)}"
   key_name="test"
+
 }
 
 output "ami"{
