da620ff9056e365b0eb3e824f5620bc141899740
ashish <itt.ashish@gmail.com>
2019-11-16 17:57:10 +0530

    install ansible

--- user-data.sh
+++ user-data.sh
@@ -1,9 +1,9 @@
 #! /bin/bash -v
 
-apt install software-properties-common
+apt install software-properties-common -y
 apt-add-repository ppa:ansible/ansible -y
-apt-get update
+apt-get update -y
 apt-get upgrade -y
-apt install python
-apt install ansible
+apt install python -y
+apt install ansible -y
 #conf ssh connection
